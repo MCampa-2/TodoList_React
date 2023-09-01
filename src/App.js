f9e53cf1a5497a0df9391cdfd363bde1e7c57404
Michael Campagnoli <michael.campagnoli@protonmail.com>
2023-08-31 22:01:04 -0400

    Cant figure out how to put line through single todo

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import './style.css';
 
 
 function App() {
-  const [todoValue, setTodoValue] = useState("");
-  const [todoItems, setTodoItems] = useState([]);
+  
+const [todos, setTodo] = useState([]);
+const [isDone, setIsDone] = useState(false);
+const ref = useRef(null);
 
-  function handleChange(e){
-    const newValue = e.target.value;
-    setTodoValue(newValue);
-  }
 
-  function addTodo(e){
-    e.preventDefault();
-    setTodoItems((prev) =>{
-      return[...prev,todoValue];
-    }); 
+function addTodo(e){
+  e.preventDefault();
+  const currentValue = ref.current.value;
+  if(currentValue){
+    setTodo((prev) =>{
+      return [...prev, currentValue]
+    });
     
-    setTodoValue("");
+    ref.current.value = ""
   }
-
-  function deleteItem(id){
-    setTodoItems((prev) =>{
-      return prev.filter((item,index) =>{
-        return index !== id
-      })
-    })
-  }
+  
+}
  
+function handleChange(){
+ setIsDone(!isDone);
+ console.log("checked")
+}
+
+
 
   return (
     <div className="container">
       <form onSubmit={addTodo}>
         <h1>Todo List</h1>
         <span>
-          <input onChange={handleChange} type="text" placeholder="add todo..." value={todoValue}></input>
+          <input ref={ref} type="text" placeholder="add todo..." ></input>
           <button type="submit">Add Todo</button>
         </span>
       </form>
       <ul>
-        {todoItems.map((item, index) => <li key={index} id={index}>{item} <button className="delete" onClick={() => deleteItem(index)}>Delete</button></li>)}
+        {todos.map((item,index) => <li key={index}><input type="checkbox" onChange={handleChange} style={{textDecoration: isDone? "line-through": "none"}}></input>{item}</li>)}
       </ul>
     </div>
   );
